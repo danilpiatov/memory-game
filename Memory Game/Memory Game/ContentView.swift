15f15a3ae7b5d57e8ba0f5b9d995ea16891fb29d
danilpiatov <dan.pyatoff2011@yandex.ru>
2022-02-14 18:48:43 +0300

    Initial commit

---
+++ Memory Game/Memory Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memory Game
+//
+//  Created by Данил Пятов on 28.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
